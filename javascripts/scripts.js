62a28e30b1ff538b863e8f3c9eaeb3ec3ebdf8de
neylshu <inanbularin05>
2026-06-30 16:36:19 +0800

    first commit

---
+++ javascripts/scripts.js
@@ -0,0 +1,15 @@
+function calc(){
+
+const num1=document.getElementbyId('n1').value;
+
+const num1=document.getElementbyId('n2').value;
+
+const number1 = perseFloat(num1);
+const number1 = perseFloat(num2);
+
+const sum = number1 + number2;
+
+document.getElementById('ans').textContent = sum;
+
+
+}
