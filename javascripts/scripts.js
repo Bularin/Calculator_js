dbf1a397fb29803510b37c78c0437b46f4e34d83
Bularin <inan.bularin@gmail.com>
2026-07-02 16:59:36 +0800

    added 3rd number

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,15 +1,15 @@
 function calc(){
 
-const num1=document.getElementbyId('n1').value;
+const num1 = document.getElementById('n1').value;
+const num2 = document.getElementById('n2').value;
+const num3 = document.getElementById('n3').value;
 
-const num1=document.getElementbyId('n2').value;
+const number1 = parseFloat(num1);
+const number2 = parseFloat(num2);
+const number3 = parseFloat(num3);
 
-const number1 = perseFloat(num1);
-const number1 = perseFloat(num2);
+const sum = number1 + number2 + number3;
 
-const sum = number1 + number2;
-
-document.getElementById('ans').textContent = sum;
-
+document.getElementById('ans'). textContent = sum;
 
 }
